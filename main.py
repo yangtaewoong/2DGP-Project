0471b9aa4ff4e9d18d6d82c4bd84221e8995e45e
yangtaewoong <tom0323@naver.com>
2024-10-27 02:43:53 +0900

    플레이 버튼 클릭 후 스테이지 선택 화면까지 구현 완료

--- main.py
+++ main.py
@@ -1,27 +1,90 @@
-from pico2d import*
+from pico2d import *
 
+# 캔버스 설정
 width, height = 1060, 800
-play_button_x, play_button_y, play_button_width, play_button_height = 200,50,300,200
-open_canvas(width,height)
-intro = load_image('resource\screen\intro.png')
+open_canvas(width, height)
 
+# 이미지 로드
+intro_image = load_image('resource/screen/intro.png')
+select_stage_image = load_image('resource/stage_select/select_map_3.png')
+
+# Play 버튼 위치 및 크기
+play_button_x, play_button_y, play_button_width, play_button_height = 200, 50, 300, 200
+
+
+# 버튼 클릭 위치 확인 함수
 def is_inside_button(x, y, button_x, button_y, button_width, button_height):
-    # 클릭 좌표(x, y)가 버튼 영역 안에 있는지 확인
     return (button_x - button_width // 2 <= x <= button_x + button_width // 2 and
             button_y - button_height // 2 <= y <= button_y + button_height // 2)
 
 
-while True:
-    clear_canvas()
-    intro.draw(width//2, height//2, width, height)
-    update_canvas()
+# Intro 상태 클래스
+class IntroState:
+    def enter(self):
+        print("IntroState: 시작 화면입니다.")
 
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            close_canvas()
-            exit()
-        elif event.type == SDL_MOUSEBUTTONDOWN:
+    def exit(self):
+        print("IntroState: 종료합니다.")
+
+    def update(self):
+        clear_canvas()
+        intro_image.draw(width // 2, height // 2, width, height)
+        update_canvas()
+
+    def handle_events(self, event, state_machine):
+        if event.type == SDL_MOUSEBUTTONDOWN:
             mouse_x, mouse_y = event.x, height - event.y
             if is_inside_button(mouse_x, mouse_y, play_button_x, play_button_y, play_button_width, play_button_height):
-                print("Play 버튼이 클릭되었습니다!")
+                print("Play 버튼이 클릭되었습니다! 다음 상태로 전환합니다.")
+                state_machine.change_state(SelectStageState())
+
+
+# SelectStage 상태 클래스
+class SelectStageState:
+    def enter(self):
+        print("SelectStageState: 스테이지 선택 화면입니다.")
+
+    def exit(self):
+        print("SelectStageState: 종료합니다.")
+
+    def update(self):
+        clear_canvas()
+        select_stage_image.draw(width // 2, height // 2, width, height)
+        update_canvas()
+
+    def handle_events(self, event, state_machine):
+        if event.type == SDL_QUIT or (event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE):
+            state_machine.running = False
+
+
+# 상태 머신 클래스
+class StateMachine:
+    def __init__(self, initial_state):
+        self.current_state = initial_state
+        self.current_state.enter()
+        self.running = True
+
+    def change_state(self, new_state):
+        self.current_state.exit()
+        self.current_state = new_state
+        self.current_state.enter()
+
+    def update(self):
+        self.current_state.update()
+
+    def handle_events(self):
+        events = get_events()
+        for event in events:
+            if event.type == SDL_QUIT:
+                self.running = False
+            else:
+                self.current_state.handle_events(event, self)
+
+
+state_machine = StateMachine(IntroState())
+
+while state_machine.running:
+    state_machine.update()
+    state_machine.handle_events()
+
+close_canvas()
--- scene.py
+++ scene.py
@@ -2,3 +2,6 @@ from pico2d import*
 from framework import*
 running = True
 
+
+
+
