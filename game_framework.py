5c824bc5c08741f28e381d08940781ec9fee0cf5
yangtaewoong <tom0323@naver.com>
2024-10-26 18:34:13 +0900

    일단 시작화면 띄워보기

---
+++ game_framework.py
@@ -0,0 +1,4 @@
+from pico2d import*
+
+
+
--- main.py
+++ main.py
@@ -3,4 +3,15 @@ import game_intro
 import game_state
 
 open_canvas(1060,800)
-clear_canvas()
+intro = load_image('resource\screen\intro.png')
+
+while True:
+    clear_canvas()
+    intro.draw(530, 400)
+    update_canvas()
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT or (event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE):
+            close_canvas()
+            exit()
