aa7b9f5293382a85484de37e55e9e93c6c529d75
yangtaewoong <tom0323@naver.com>
2024-11-15 13:59:35 +0900

    스테이트머신 구현

--- character.py
+++ character.py
@@ -1,6 +1,52 @@
 from pico2d import *
 import game_world
 import framework
+from state_machine import*
+
+PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
+RUN_SPEED_KMPH = 20.0  # Km / Hour
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
+class Idle:
+    pass
+
 
 class Character:
-    pass
+    def __init__(self):
+        self.x, self.y = 400, 90
+        self.face_dir = 1
+        self.ball_count = 10
+        self.font = load_font('ENCR10B.TTF', 16)
+        self.image = load_image('animation_sheet.png')
+        self.state_machine = StateMachine(self)
+        self.state_machine.start(Idle)
+        self.state_machine.set_transitions(
+            {
+                Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, time_out: Sleep, space_down: Idle},
+                Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle, space_down: Run},
+                Sleep: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, space_down: Idle}
+            }
+        )
+
+    def update(self):
+        self.state_machine.update()
+
+    def handle_event(self, event):
+        self.state_machine.add_event(('INPUT', event))
+        pass
+
+    def draw(self):
+        self.state_machine.draw()
+
+    def get_bb(self):
+        #return self.x - 20, self.y - 50, self.x + 20, self.y + 50
+        pass
+
+    def handle_collision(self, group, other):
+
+        pass
